b20a85e66e43875a8d0023b90e4ab99e879d0518
heyongxian <1064042411@qq.com>
2018-06-14 12:07:22 +0800

    add listener

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,9 +1,12 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ChatClient extends Frame {
 	TextField tfTxt = new TextField();
+
 	TextArea taContent = new TextArea();
 
 	public static void main(String[] args){
@@ -16,12 +19,20 @@ public class ChatClient extends Frame {
 		add(tfTxt,BorderLayout.SOUTH);
 		add(taContent,BorderLayout.NORTH);
 		pack();
-		setVisible(true);
 		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
+		tfTxt.addActionListener(new TextFieldListener());
+		setVisible(true);
+	}
+
+	private class TextFieldListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			String s = tfTxt.getText().trim();
+			taContent.setText(s);
+		}
 	}
 }
