14ee6d77aa00ec1992d82c61cfc0f746eb11cd08
heyongxian <1064042411@qq.com>
2018-06-14 11:56:48 +0800

    add closing

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ChatClient extends Frame {
 	TextField tfTxt = new TextField();
@@ -15,5 +17,11 @@ public class ChatClient extends Frame {
 		add(taContent,BorderLayout.NORTH);
 		pack();
 		setVisible(true);
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 	}
 }
